64b678714b334fe9e66ce738a32586a9f76f78ac
tuxtor <tuxtor@shekalug.org>
2013-02-14 12:08:41 -0200

    Creating master branch and makefile structure

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: tuxtor
+ *
+ * Created on February 14, 2013, 11:18 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
