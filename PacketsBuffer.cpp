32a8a7f1edf2d7efa70c96bfb9a9d8043ffe6b24
tuxtor <tuxtor@shekalug.org>
2013-02-19 19:21:27 -0300

    Freeing source code under GPLv3

--- PacketsBuffer.cpp
+++ PacketsBuffer.cpp
@@ -1,8 +1,20 @@
 /* 
- * File:   PacketsBuffer.cpp
- * Author: tuxtor
+ * Copyright (C) 2013  Víctor Orozco
  * 
- * Created on February 18, 2013, 11:11 AM
+ * This file is part of csniffstat.
+ * 
+ * csniffstat program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * csniffstat program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "PacketsBuffer.h"
